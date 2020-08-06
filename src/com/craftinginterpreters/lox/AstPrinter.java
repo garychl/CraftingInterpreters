2d25c77bb2979105c2572ff1ebab462107c12179
garychl <garychl320@gmail.com>
2020-08-06 10:20:44 +0800

    add chapter 5

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -40,27 +40,27 @@ class AstPrinter implements Expr.Visitor<String> {
         return builder.toString();
     }
 
-
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-                new Expr.Grouping(
-                        new Expr.Binary(
-                                new Expr.Literal(1),
-                                new Token(TokenType.PLUS, "+", null, 1),
-                                new Expr.Literal(2)
-                        )
-                ),
-                new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Grouping(
-                        new Expr.Binary(
-                                new Expr.Literal(4),
-                                new Token(TokenType.MINUS, "-", null, 1),
-                                new Expr.Literal(3)
-                        )
-                )
-        );
-
-        System.out.println(new AstPrinter().print(expression));
-    }
+//    //for testing
+//    public static void main(String[] args) {
+//        Expr expression = new Expr.Binary(
+//                new Expr.Grouping(
+//                        new Expr.Binary(
+//                                new Expr.Literal(1),
+//                                new Token(TokenType.PLUS, "+", null, 1),
+//                                new Expr.Literal(2)
+//                        )
+//                ),
+//                new Token(TokenType.STAR, "*", null, 1),
+//                new Expr.Grouping(
+//                        new Expr.Binary(
+//                                new Expr.Literal(4),
+//                                new Token(TokenType.MINUS, "-", null, 1),
+//                                new Expr.Literal(3)
+//                        )
+//                )
+//        );
+//
+//        System.out.println(new AstPrinter().print(expression));
+//    }
 
 }
